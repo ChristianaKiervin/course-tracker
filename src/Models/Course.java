5d39e1e69ef8bdb924990c666960623e0838e54f
Christiana Kiervin <christianakiervin@gmail.com>
2021-08-26 20:24:07 -0400

    Updated readme

---
+++ src/Models/Course.java
@@ -0,0 +1,62 @@
+package Models;
+
+/**
+ *
+ * @author snadi
+ * 
+ */
+public class Course  {
+    
+    private String code;
+    private double grade;
+    private double maxGrade;
+    
+    public Course(String code, double grade, double maxGrade) {
+        setCode(code);
+        setGrade(grade);
+        setMaxGrade(maxGrade);
+    }
+    
+    public String getCode() {
+        return code;
+    }
+    
+    public void setCode(String code) {
+        if (code != null && !code.trim().isEmpty() && code.matches("[a-zA-Z]{4}\\d{5}"))
+            this.code = code;
+        else
+            throw new IllegalArgumentException("Course code can't be empty");
+    }
+    
+    public double getGrade() {
+        return grade;
+    }
+    
+    public void setGrade(double grade) {
+        if (grade >= 0)
+            this.grade = grade;
+        else
+            throw new IllegalArgumentException("Grade can't be a negative value.");
+    }
+    
+    public double getMaxGrade() {
+        return maxGrade;
+    }
+    
+    public void setMaxGrade(double maxGrade) {
+       if (maxGrade >= 0)
+            this.maxGrade = maxGrade;
+        else
+            throw new IllegalArgumentException("Grade can't be a negative value.");
+    }
+    @Override
+    public String toString() {
+        return String.format("%s", code);
+    }
+    
+    // todo: hash
+    
+
+   
+    
+}
